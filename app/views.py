e09ba9be879b4d442a1c4987f8a35c5172d453e2
mattbford <47286642+mattbford@users.noreply.github.com>
2019-11-14 12:14:20 -0700

    test

--- app/views.py
+++ app/views.py
@@ -8,6 +8,14 @@ def home(request):
     # this query is the search for the summoner name. This function servers as the root for all logic involving analytics.
     # the search will gather your stats for games using that champ and train itself using generalized stats and comparing
     # the results to your games.
+    #
+    #Steps:
+    #1. Scrape lol API for summoner names for each league (skill difference in division is likely arbitrary) focus on master/grandmaster
+    #   try to massage into CSV
+    #2. Search name and get most recent game. 
+
+
+
     query = ""
     if request.GET:
         query = request.GET['q']
